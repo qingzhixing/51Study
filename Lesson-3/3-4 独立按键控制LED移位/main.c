a8586dfd044cc7e80e3d83e377f9beb294c7bd15
qingzhixing <qzsptmail@protonmail.com>
2023-07-19 01:35:42 +0800

    add stdint.h

--- Lesson-3/3-4 独立按键控制LED移位/main.c
+++ Lesson-3/3-4 独立按键控制LED移位/main.c
@@ -1,4 +1,25 @@
+#include "defines.h"
+#include "utilities.h"
 #include <REGX52.h>
-void main(){
+void main()
+{
+    uint8_t LEDOnLightId = 0;
+    while (TRUE)
+    {
+        if (P3_1 == 0)
+        {
+            Sleep(20);
+            while (P3_1 == 0)
+            {
+            }
+            Sleep(20);
 
+            LEDOnLightId++;
+            if (LEDOnLightId >= 8)
+            {
+                LEDOnLightId = 0;
+            }
+            P2 = ~(0x01 << LEDOnLightId);
+        }
+    }
 }
