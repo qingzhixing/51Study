551cd43dae11aa028c2bb9716a2c4146998f9d13
qingzhixing <qzsptmail@protonmail.com>
2023-07-19 01:50:20 +0800

    添加编译设置 完成独立按键控制LED移位

--- Lesson-3/3-4 独立按键控制LED移位/main.c
+++ Lesson-3/3-4 独立按键控制LED移位/main.c
@@ -3,9 +3,26 @@
 #include <REGX52.h>
 void main()
 {
-    uint8_t LEDOnLightId = 0;
+    int8_t LEDOnLightId = 0;
     while (TRUE)
     {
+        // right
+        if (P3_3 == 0)
+        {
+            Sleep(20);
+            while (P3_3 == 0)
+            {
+            }
+            Sleep(20);
+
+            LEDOnLightId++;
+            if (LEDOnLightId > 7)
+            {
+                LEDOnLightId = 0;
+            }
+        }
+
+        // left
         if (P3_1 == 0)
         {
             Sleep(20);
@@ -14,12 +31,12 @@ void main()
             }
             Sleep(20);
 
-            LEDOnLightId++;
-            if (LEDOnLightId >= 8)
+            LEDOnLightId--;
+            if (LEDOnLightId < 0)
             {
-                LEDOnLightId = 0;
+                LEDOnLightId = 7;
             }
-            P2 = ~(0x01 << LEDOnLightId);
         }
+        P2 = ~(0x01 << LEDOnLightId);
     }
 }
