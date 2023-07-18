da06020f89abb9be1bb70c530cf9aacb18abe895
qingzhixing <qzsptmail@protonmail.com>
2023-07-19 01:38:15 +0800

    fix stdint

--- Lesson-3/3-4 独立按键控制LED移位/stdint.h
+++ Lesson-3/3-4 独立按键控制LED移位/stdint.h
@@ -1,18 +1,13 @@
-#ifndef C4B323C3_39EB_4650_AC70_27ACE5BFB96A
-#define C4B323C3_39EB_4650_AC70_27ACE5BFB96A
 #ifndef __STDINT_H
 #define __STDINT_H
 typedef signed char int8_t;
 typedef signed short int int16_t;
 typedef signed int int32_t;
-typedef signed long long int int64_t;
+typedef signed long int int64_t;
 typedef unsigned char uint8_t;
 typedef unsigned short int uint16_t;
 typedef unsigned int uint32_t;
-typedef unsigned long long int uint64_t;
+typedef unsigned long int uint64_t;
 typedef uint8_t bool;
 
 #endif /* __STDINT_H */
-
-
-#endif /* C4B323C3_39EB_4650_AC70_27ACE5BFB96A */
--- template/stdint.h
+++ template/stdint.h
@@ -3,11 +3,11 @@
 typedef signed char int8_t;
 typedef signed short int int16_t;
 typedef signed int int32_t;
-typedef signed long long int int64_t;
+typedef signed long int int64_t;
 typedef unsigned char uint8_t;
 typedef unsigned short int uint16_t;
 typedef unsigned int uint32_t;
-typedef unsigned long long int uint64_t;
+typedef unsigned long int uint64_t;
 typedef uint8_t bool;
 
 #endif /* __STDINT_H */
