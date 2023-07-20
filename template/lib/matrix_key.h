4e93e3abededd45f92c72c19c821e1fd50479567
qingzhixing <qzsptmail@protonmail.com>
2023-07-20 21:21:21 +0800

    删去vscode自动添加前缀

--- template/lib/matrix_key.h
+++ template/lib/matrix_key.h
@@ -1,5 +1,3 @@
-#ifndef F817366C_2FD3_4BB9_84E1_24CC56041795
-#define F817366C_2FD3_4BB9_84E1_24CC56041795
 #ifndef __LIB_MATRIX_KEY_H
 #define __LIB_MATRIX_KEY_H
 
@@ -8,6 +6,3 @@ uint8_t MatrixKey_Scan(void);
 uint8_t key_matrix_ranks_scan(void);
 
 #endif /* __LIB_MATRIX_KEY_H */
-
-
-#endif /* F817366C_2FD3_4BB9_84E1_24CC56041795 */
