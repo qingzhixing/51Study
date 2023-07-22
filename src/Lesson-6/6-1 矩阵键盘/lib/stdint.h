fef0a2fdfc64c484520eaf465ef0d8cbb26ccc87
qingzhixing <qzsptmail@protonmail.com>
2023-07-22 16:36:22 +0800

    修正stdint范围

--- src/Lesson-6/6-1 矩阵键盘/lib/stdint.h
+++ src/Lesson-6/6-1 矩阵键盘/lib/stdint.h
@@ -1,13 +1,11 @@
 #ifndef __LIB_STDINT_H
 #define __LIB_STDINT_H
 typedef signed char int8_t;
-typedef signed short int int16_t;
-typedef signed int int32_t;
-typedef signed long int int64_t;
+typedef signed int int16_t;
+typedef signed long int int32_t;
 typedef unsigned char uint8_t;
-typedef unsigned short int uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long int uint64_t;
+typedef unsigned int uint16_t;
+typedef unsigned long int uint32_t;
 typedef uint8_t bool;
 
 #endif /* __LIB_STDINT_H */
--- src/Lesson-6/6-2 矩阵键盘密码锁/main.c
+++ src/Lesson-6/6-2 矩阵键盘密码锁/main.c
@@ -2,17 +2,24 @@
 #include "lib/defines.h"
 #include "lib/matrix_key.h"
 #include "lib/utilities.h"
+
+// 这里定义功能按键
+
+uint8_t scanValue = 0;
+uint8_t password[4] = {0};
 void main()
 {
-    int scanValue = 0;
-    int lastButtonDown = 0;
-
     LCD_Init();
+    LCD_ShowString(1, 1, "Password:");
     while (TRUE)
     {
-        LCD_ShowString(2, 6, "Entered");
         scanValue = MatrixKey_Scan();
-        lastButtonDown = scanValue == 0 ? lastButtonDown : scanValue;
-        LCD_ShowNum(1, 1, lastButtonDown, 2);
+        if (scanValue != 0)
+        {
+            // 1 ~ 10 号按键我们定义为密码区，表示 1~9,0
+            if (scanValue <= 10)
+            {
+            }
+        }
     }
 }
